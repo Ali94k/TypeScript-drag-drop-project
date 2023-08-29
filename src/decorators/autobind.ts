e295f0940fd0314caeb3682a11f636af5663b60f
Ali <alikerimli94@gmail.com>
2023-08-29 21:40:19 +0200

    split into files with "reference"

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+  // autobind decorator
+  export function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+    const origianlMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = origianlMethod.bind(this)
+        return boundFn
+      },
+    }
+    return adjDescriptor
+  }
+}
